77dff99ee83f23c050f2f4bb80958767e1c63c10
Rob Lewis <rob.y.lewis@gmail.com>
2022-09-08 14:11:46 -0400

    class compiler: status after Sep 7

---
+++ lib/compile.ml
@@ -0,0 +1,26 @@
+open S_exp
+
+exception BadExpression of s_exp
+
+let compile (program : s_exp) : string =
+  match program with
+  | Num n ->
+      String.concat "\n"
+        [
+          "global _entry"; "_entry:"; Printf.sprintf "\tmov rax, %d" n; "\tret";
+        ]
+  | e -> raise (BadExpression e)
+
+let compile_to_file (program : string) : unit =
+  let file = open_out "program.s" in
+  output_string file (compile (parse program));
+  close_out file
+
+let compile_and_run (program : string) : string =
+  compile_to_file program;
+  ignore (Unix.system "nasm program.s -f macho64 -o program.o");
+  ignore (Unix.system "gcc program.o runtime.o -o program");
+  let inp = Unix.open_process_in "./program" in
+  let r = input_line inp in
+  close_in inp;
+  r
