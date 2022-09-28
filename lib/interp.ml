d6a617fa2dd72ae69a29928b696157df8a58e804
Rob Lewis <rob.y.lewis@gmail.com>
2022-09-28 10:55:47 -0400

    after class 9/28

--- lib/interp.ml
+++ lib/interp.ml
@@ -1,4 +1,5 @@
 open S_exp
+open Util
 
 type value = Number of int | Boolean of bool
 
@@ -7,42 +8,46 @@ let string_of_val (v : value) : string =
 
 exception BadExpression of s_exp
 
-let rec interp_exp (exp : s_exp) : value =
+let rec interp_exp (env : value symtab) (exp : s_exp) : value =
   match exp with
+  | Sym var when Symtab.mem var env -> Symtab.find var env
   | Num n -> Number n
   | Sym "true" -> Boolean true
   | Sym "false" -> Boolean false
   | Lst [ Sym "add1"; arg ] -> (
-      match interp_exp arg with
+      match interp_exp env arg with
       | Number n -> Number (n + 1)
       | _ -> raise (BadExpression exp))
   | Lst [ Sym "sub1"; arg ] -> (
-      match interp_exp arg with
+      match interp_exp env arg with
       | Number n -> Number (n - 1)
       | _ -> raise (BadExpression exp))
   | Lst [ Sym "not"; arg ] ->
-      if interp_exp arg = Boolean false then Boolean true else Boolean false
+      if interp_exp env arg = Boolean false then Boolean true else Boolean false
   | Lst [ Sym "num?"; arg ] -> (
-      match interp_exp arg with Number _ -> Boolean true | _ -> Boolean false)
+      match interp_exp env arg with Number _ -> Boolean true | _ -> Boolean false)
   | Lst [ Sym "zero?"; arg ] ->
-      if interp_exp arg = Number 0 then Boolean true else Boolean false
+      if interp_exp env arg = Number 0 then Boolean true else Boolean false
   | Lst [ Sym "if"; test_exp; then_exp; else_exp ] ->
-      if interp_exp test_exp = Boolean false then interp_exp else_exp
-      else interp_exp then_exp
+      if interp_exp env test_exp = Boolean false then interp_exp env else_exp
+      else interp_exp env then_exp
   | Lst [ Sym "+"; e1; e2 ] -> (
-      match (interp_exp e1, interp_exp e2) with
+      match (interp_exp env e1, interp_exp env e2) with
       | Number n1, Number n2 -> Number (n1 + n2)
       | _ -> raise (BadExpression exp))
   | Lst [ Sym "-"; e1; e2 ] -> (
-      match (interp_exp e1, interp_exp e2) with
+      match (interp_exp env e1, interp_exp env e2) with
       | Number n1, Number n2 -> Number (n1 - n2)
       | _ -> raise (BadExpression exp))
-  | Lst [ Sym "="; e1; e2 ] -> Boolean (interp_exp e1 = interp_exp e2)
+  | Lst [ Sym "="; e1; e2 ] -> Boolean (interp_exp env e1 = interp_exp env e2)
   | Lst [ Sym "<"; e1; e2 ] -> (
-      match (interp_exp e1, interp_exp e2) with
+      match (interp_exp env e1, interp_exp env e2) with
       | Number n1, Number n2 -> Boolean (n1 < n2)
       | _ -> raise (BadExpression exp))
+  | Lst [ Sym "let"; Lst [ Lst [ Sym var; e ]]; body ] -> 
+      let e_value = interp_exp env e in 
+      interp_exp (Symtab.add var e_value env) body
   | _ -> raise (BadExpression exp)
 
 let interp (program : string) : string =
-  parse program |> interp_exp |> string_of_val
+  parse program |> interp_exp Symtab.empty |> string_of_val
