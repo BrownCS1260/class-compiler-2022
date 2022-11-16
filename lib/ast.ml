e7d0f403ced94a6b99e070a0f11b8b6659d2c725
Rob Lewis <rob.y.lewis@gmail.com>
2022-11-16 08:07:35 -0500

    after class Nov 14

--- lib/ast.ml
+++ lib/ast.ml
@@ -41,7 +41,7 @@ type expr =
   | Do of expr list
   | Num of int
   | Var of string
-  | Call of string * expr list
+  | Call of expr * expr list
   | True
   | False
 
@@ -71,7 +71,8 @@ let rec expr_of_s_exp : s_exp -> expr = function
         ( Option.get (prim2_of_string prim),
           expr_of_s_exp arg1,
           expr_of_s_exp arg2 )
-  | Lst (Sym f :: args) -> Call (f, List.map expr_of_s_exp args)
+  | Lst (Sym f :: args) -> Call (Var f, List.map expr_of_s_exp args)
+  | Lst (e :: args) -> Call (expr_of_s_exp e, List.map expr_of_s_exp args)
   | e -> raise (BadSExpression e)
 
 let program_of_s_exps (exps : s_exp list) : program =
